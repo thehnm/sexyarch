1a401aea6bfd7324d86a1c63c42229408c6b8acd
thehnm <thehnm@posteo.net>
2021-01-31 15:40:57 +0100

    Restructure and debug

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 PURPLE='\033[0;35m'
+CYAN='\033[1;36m'
 NC='\033[0m' # No Color
 
 err() {
@@ -24,7 +25,7 @@ succ() {
 }
 
 info1() {
-    printf "${PURPLE}$1${NC}\n"
+    printf "${CYAN}$1${NC}\n"
 }
 
 info2() {
@@ -37,6 +38,7 @@ info3() {
 
 warn() {
     printf "${ORANGE}WARNING! $1${NC}\n"
+    yesnodialog "${ORANGE}Do you really want to continue?${NC}" "" "exit 1"
 }
 
 yesnodialog() {
@@ -55,9 +57,16 @@ infodialog() {
     yesnodialog "Do you want to continue?" "" "exit 1"
 }
 
+queue() {
+for command in "$@"; do
+    printf "\n"
+    eval "$command"
+done
+}
+
 initialcheck() {
     info2 "Initial check"
-    pacman -S --noconfirm --needed git &>/dev/null || { err "You are not running this script as root."; exit 1; }
+    [ $debug = 0 ] && { pacman -S --noconfirm --needed git &>/dev/null || { err "You are not running this script as root."; exit 1; } }
 }
 
 preinstallmsg() {
@@ -65,49 +74,52 @@ preinstallmsg() {
 }
 
 settimezone() {
-    info2 "Setting timezone"
     while true; do
         read -p "Please enter your continent: " continent
         read -p "Please enter an appropriate city from your timezone: " city
-        ln -sf /usr/share/zoneinfo/"$continent"/"$city" /etc/localtime &>/dev/null
-        [ ! -e /etc/localtime ] && err "Please enter correct values!" && continue
-        hwclock --systohc
+        info2 "Setting timezone"
+        if [ $debug = 0 ]; then
+            ln -sf /usr/share/zoneinfo/"$continent"/"$city" /etc/localtime &>/dev/null
+            [ ! -e /etc/localtime ] && err "Please enter correct values!" && continue
+            hwclock --systohc
+        fi
         break
     done
 }
 
 genlocale() {
-    info2 "Generating locale"
     yesnodialog "The following locale will be set: en_US\nDo you want to keep this?\nOtherwise, edit /etc/locale.gen directly." "" "read -p 'Enter locale to use: ' locale"
+    info2 "Generating locale"
     [ "$yn" = "y" ] && locale="en_US"
-    sed -i "s/\#en_US/en_US/" /etc/locale.gen
-    locale-gen
-    info2 "Setting locale"
-    echo "LANG=$locale.UTF-8" > /etc/locale.conf
-    echo "LC_ALL=$locale.UTF-8" >> /etc/locale.conf
+    if [ $debug = 0 ]; then
+        sed -i "s/\#en_US/en_US/" /etc/locale.gen
+        locale-gen
+        info2 "Setting locale"
+        echo "LANG=$locale.UTF-8" > /etc/locale.conf
+        echo "LC_ALL=$locale.UTF-8" >> /etc/locale.conf
+    fi
 }
 
 sethostname() {
-    info2 "Setting hostname"
     read -p "Please enter your hostname: " hostname
-    echo "$hostname" > /etc/hostname
-    echo "127.0.0.1 localhost" > /etc/hosts
-    echo "::1 localhost" >> /etc/hosts
-    echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+    info2 "Setting hostname"
+    if [ $debug = 0 ]; then
+        echo "$hostname" > /etc/hostname
+        echo "127.0.0.1 localhost" > /etc/hosts
+        echo "::1 localhost" >> /etc/hosts
+        echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
+    fi
 }
 
 installfullsystem() {
-    info2 "Configure full system"
-    yesnodialog "This script will set these settings:\n- Timezone = Europe/Berlin\n- Locale = en_US\nDo you wish to configure these?" "settimezone;genlocale"
+    yesnodialog "This script will set these settings:\n- Timezone = Europe/Berlin\n- Locale = en_US\nDo you wish to configure these?" "queue settimezone genlocale"
 }
 
 islaptop() {
-    info2 "Laptop configuration"
     yesnodialog "Do you install this on a laptop?" "laptop=1" "laptop=0"
 }
 
 getuserandpass() {
-    info2 "Creating user"
     read -p "Please enter your username: " name
     namere="^[a-z_][a-z0-9_-]*$"
     while ! [[ "${name}" =~ ${namere} ]]; do
@@ -129,26 +141,32 @@ getuserandpass() {
         printf "\n"
     done
 }
+
 usercheck() {
-    ! (id -u $name &>/dev/null) || infodialog "$(warn "User \'$name\' already exits. The following steps will overwrite the user's password and settings")"
+    #! (id -u $name &>/dev/null) || { infodialog "$(warn "User \'$name\' already exits. The following steps will overwrite the user's password and settings")" }
+    warn "User \'$name\' already exits. The following steps will overwrite the user's password and settings"
 }
 
 adduserandpass() { \
     # Adds user `$name` with password $pass1.
     info2 "Add user \'$name\'"
-    useradd -m -g wheel -s /bin/zsh "$name" &>/dev/null ||
-    usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
-    usermod -a -G video "$name"
-    repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel $(dirname "$repodir")
-    echo "$name:$pass1" | chpasswd
+    if [ $debug = 0 ]; then
+        useradd -m -g wheel -s /bin/zsh "$name" &>/dev/null ||
+        usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+        usermod -a -G video "$name"
+        repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel $(dirname "$repodir")
+        echo "$name:$pass1" | chpasswd
+    fi
     unset pass1 pass2 ;
 }
 
 newperms() { \
     # Set special sudoers settings for install (or after).
     info2 "Setting sudoers"
-    sed -i "/#SCRIPT/d" /etc/sudoers
-    echo -e "$@ #SCRIPT" >> /etc/sudoers
+    if [ $debug = 0 ]; then
+        sed -i "/#SCRIPT/d" /etc/sudoers
+        echo -e "$@ #SCRIPT" >> /etc/sudoers
+    fi
 }
 
 ###############################################################################
@@ -161,42 +179,46 @@ downloadandeditpackages() { \
 
 refreshkeys() { \
     info2 "Refreshing Arch Linux Keyring"
-    pacman --noconfirm -Sy archlinux-keyring &>/dev/null
+    [ $debug = 0 ] && pacman --noconfirm -Sy archlinux-keyring &>/dev/null
 }
 
 installyay() { \
     info2 "Installing yay"
-    if [ ! -f /usr/bin/yay ]; then
-        pacman --noconfirm -S git &>/dev/null
-        sudo -u $name git clone https://aur.archlinux.org/yay.git /tmp/yay &>/dev/null
-        cd /tmp/yay
-        sudo -u $name makepkg --noconfirm -si &>/dev/null
+    if [ $debug = 0 ]; then
+        if [ ! -f /usr/bin/yay ]; then
+            pacman --noconfirm -S git &>/dev/null
+            sudo -u $name git clone https://aur.archlinux.org/yay.git /tmp/yay &>/dev/null
+            cd /tmp/yay
+            sudo -u $name makepkg --noconfirm -si &>/dev/null
+        fi
     fi
 }
 
 pacmaninstall() { \
     info2 "Install $1. $2"
-    pacman --noconfirm --needed -S "$1" &>/dev/null
+    [ $debug = 0 ] && pacman --noconfirm --needed -S "$1" &>/dev/null
 }
 
 looppacmaninstall() {
     info2 "[$n/$total] $1 ($2)"
-    pacman --noconfirm --needed -S "$1" &>/dev/null
+    [ $debug = 0 ] && pacman --noconfirm --needed -S "$1" &>/dev/null
 }
 
 loopaurinstall() { \
     info2 "[$n/$total] $1 ($2)"
-    yes | sudo -u $name yay --noconfirm -S "$1" &>/dev/null
+    [ $debug = 0 ] && yes | sudo -u $name yay --noconfirm -S "$1" &>/dev/null
 }
 
 putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-    info3 "Downloading $1"
-    [ -z "$3" ] && branch="master" || branch="$3"
-    tempdir=$(mktemp -d)
-    [ ! -d "$2" ] && mkdir -p "$2"
-    chown -R "$name":wheel "$tempdir" "$2"
-    sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$tempdir" >/dev/null 2>&1
-    sudo -u "$name" cp -rfT "$tempdir" "$2"
+    #info2 "Downloading $1"
+    if [ $debug = 0 ]; then
+        [ -z "$3" ] && branch="master" || branch="$3"
+        tempdir=$(mktemp -d)
+        [ ! -d "$2" ] && mkdir -p "$2"
+        chown -R "$name":wheel "$tempdir" "$2"
+        sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$tempdir" >/dev/null 2>&1
+        sudo -u "$name" cp -rfT "$tempdir" "$2"
+    fi
 }
 
 # Requires the git repository to have some kind of build file/Makefile
@@ -204,20 +226,24 @@ loopgitinstall() {
     progname="$(basename "$1" .git)"
     dir="$repodir/$progname"
     info2 "[$n/$total] $1 ($2)"
-    putgitrepo "$1" "$dir"
-    cd "$dir" || exit
-    make install >/dev/null 2>&1
-    cd "$currentdir" || return ;
+    if [ $debug = 0 ]; then
+        putgitrepo "$1" "$dir"
+        cd "$dir" || exit
+        make install >/dev/null 2>&1
+        cd "$currentdir" || return ;
+    fi
 }
 
 setup_libinput() { \
-    info2 "Configure libinput for laptops"
     pacmaninstall "libinput" "Input device management and event handling library"
-    ln -s /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
-    if [ -f configs/40-libinput.conf ]; then
-        cp configs/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
-    else
-        curl https://raw.githubusercontent.com/thehnm/tarbs/master/configs/40-libinput.conf > /usr/share/X11/xorg.conf.d/40-libinput.conf
+    info2 "Configure libinput for laptops"
+    if [ $debug = 0 ]; then
+        ln -s /usr/share/X11/xorg.conf.d/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
+        if [ -f configs/40-libinput.conf ]; then
+            cp configs/40-libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
+        else
+            curl https://raw.githubusercontent.com/thehnm/tarbs/master/configs/40-libinput.conf > /usr/share/X11/xorg.conf.d/40-libinput.conf
+        fi
     fi
 }
 
@@ -234,110 +260,115 @@ install() {
     total=$(wc -l < packages.csv)
     #aurinstalled=$(pacman -Qm | awk '{print $1}')
     while IFS=, read -r tag program comment; do
-        n=$((n+1))
         case "$tag" in
             "") looppacmaninstall "$program" "$comment" ;;
             "A") loopaurinstall "$program" "$comment" ;;
             "G") loopgitinstall "$program" "$comment" ;;
         esac
+        n=$((n+1))
     done < packages.csv ;
 }
 
 serviceinit() {
     for service in "$@"; do
         info2 "Enabling \"$service\""
-        systemctl enable "$service" &>/dev/null
-        systemctl start "$service" &>/dev/null
+        if [ $debug = 0 ]; then
+            systemctl enable "$service" &>/dev/null
+            systemctl start "$service" &>/dev/null
+        fi
     done
 }
 
 installantibody() {
     info2 "Install antibody zsh plugin manager"
-    sudo -u $name curl -sfL git.io/antibody | sh -s - -b /home/$name/.local/bin/ &>/dev/null
+    [ $debug = 0 ] && sudo -u $name curl -sfL git.io/antibody | sh -s - -b /home/$name/.local/bin/ &>/dev/null
 }
 
 installdotfiles() {
+    info2 "Installing dotfiles"
     putgitrepo "$dotfilesrepo" "/home/$name"
-    cd /home/"$name" && sudo -u "$name" git config --local status.showUntrackedFiles no && sudo -u "$name" git update-index --assume-unchanged README.md && rm README.md
+    [ $debug = 0 ] && cd /home/"$name" && sudo -u "$name" git config --local status.showUntrackedFiles no && sudo -u "$name" git update-index --assume-unchanged README.md && rm README.md
 }
 
 systembeepoff() {
     info2 "Disabling beep sound"
-    rmmod pcspkr
-    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+    if [ $debug = 0 ]; then
+        rmmod pcspkr
+        echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+    fi
 }
 
 resetpulse() { \
     info2 "Resetting Pulseaudio"
-    killall pulseaudio &>/dev/null
-    sudo -u "$name" pulseaudio --start
+    if [ $debug = 0 ]; then
+        killall pulseaudio &>/dev/null
+        sudo -u "$name" pulseaudio --start
+    fi
 }
 
 miscellaneous() {
     info2 "Setting miscellaneous stuff"
 
-    ln -sf /usr/bin/dash /bin/sh
+    [ $debug = 0 ] && ln -sf /usr/bin/dash /bin/sh
 
     systembeepoff
 
     # Pulseaudio, if/when initially installed, often needs a restart to work immediately.
     [[ -f /usr/bin/pulseaudio ]] && resetpulse
 
-    # Color pacman
-    sed -i "s/^#Color/Color/g" /etc/pacman.conf
-    # Fix audio problem
-    sed -i 's/^ autospawn/; autospawn/g' /etc/pulse/client.conf
+    if [ $debug = 0 ]; then
+        # Color pacman
+        sed -i "s/^#Color/Color/g" /etc/pacman.conf
+        # Fix audio problem
+        sed -i 's/^ autospawn/; autospawn/g' /etc/pulse/client.conf
 
-    # Create configuration directories
-    sudo -u "$name" mkdir -p /home/"$name"/.config/zsh ## Stores the zshrc
-    sudo -u "$name" mkdir -p /home/"$name"/.local/share/zsh ## Stores history file for zsh
-    sudo -u "$name" mkdir -p /home/"$name"/.config/notmuch ## Required by mutt-wizard
-    sudo -u "$name" mkdir -p /home/"$name"/.config/newsboat ## Stores newsboat config
-    sudo -u "$name" mkdir -p /home/"$name"/.local/share/newsboat ## Stores the cache and history file
+        # Create configuration directories
+        sudo -u "$name" mkdir -p /home/"$name"/.config/zsh ## Stores the zshrc
+        sudo -u "$name" mkdir -p /home/"$name"/.local/share/zsh ## Stores history file for zsh
+        sudo -u "$name" mkdir -p /home/"$name"/.config/notmuch ## Required by mutt-wizard
+        sudo -u "$name" mkdir -p /home/"$name"/.config/newsboat ## Stores newsboat config
+        sudo -u "$name" mkdir -p /home/"$name"/.local/share/newsboat ## Stores the cache and history file
 
-    # Create XDG user directories
-    sudo -u "$name" mkdir -p /home/"$name"/dl # Download directory
-    sudo -u "$name" mkdir -p /home/"$name"/docs
-    sudo -u "$name" mkdir -p /home/"$name"/music
-    sudo -u "$name" mkdir -p /home/"$name"/pics
+        # Create XDG user directories
+        sudo -u "$name" mkdir -p /home/"$name"/dl # Download directory
+        sudo -u "$name" mkdir -p /home/"$name"/docs
+        sudo -u "$name" mkdir -p /home/"$name"/music
+        sudo -u "$name" mkdir -p /home/"$name"/pics
+    fi
+}
+
+cleanup() {
+    printf "\n"
+    unset pass1 pass2
+    err "Installation aborted"
+    exit 1
 }
 
 ###############################################################################
 
+trap "cleanup" SIGINT SIGTERM
+
+debug=1
+
 currentdir=$(pwd)
 
-initialcheck
+clear
 
-sethostname
-
-installfullsystem
-
-getuserandpass
-
-usercheck
-
-preinstallmsg
-
-adduserandpass
-
-newperms "%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay"
-
-islaptop
-
-downloadandeditpackages "$currentdir"
-
-refreshkeys
-
-installyay || { err "yay has to be installed to continue"; exit; }
-
-install "$currentdir"
-
-installdotfiles
-
-installantibody
-
-serviceinit NetworkManager cronie ntpdate ssh
-
-miscellaneous
-
-succ "Installation is done. You can reboot now"
+queue "initialcheck" \
+      "sethostname" \
+      "installfullsystem" \
+      "getuserandpass" \
+      "usercheck" \
+      "preinstallmsg" \
+      "adduserandpass" \
+      "newperms \"%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay\"" \
+      "islaptop" \
+      "downloadandeditpackages $currentdir" \
+      "refreshkeys" \
+      "installyay || { err 'yay has to be installed to continue'; exit 1; }" \
+      "install $currentdir" \
+      "installdotfiles" \
+      "installantibody" \
+      "serviceinit NetworkManager cronie ntpdate ssh" \
+      "miscellaneous" \
+      "succ 'Installation is done. You can reboot now'"
