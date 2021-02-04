5078b681928eb4d03f034f230c93f12dd4762489
thehnm <thehnm@posteo.net>
2021-02-04 16:37:26 +0100

    Fix typo ssh

--- install.sh
+++ install.sh
@@ -403,7 +403,7 @@ queue "initialcheck" \
       "installdotfiles" \
       "installantibody" \
       "newperms \"%wheel ALL=(ALL) ALL\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay\"" \
-      "serviceinit NetworkManager cronie ntpdate ssh" \
+      "serviceinit NetworkManager cronie ntpdate sshd" \
       "miscellaneous" \
       "settimezone" \
       "genlocale" \
